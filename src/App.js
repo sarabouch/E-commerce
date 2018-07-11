8590e238dcd431c6d703031fe7b75375ea69432d
Sarabouch <sara93bo@hotmail.com>
2018-07-11 15:46:39 +0200

    router added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Link} from "react-router-dom"
+import {BrowserRouter, Route, Link, Switch} from "react-router-dom"
 import styled from "styled-components"
 
 import {shoes} from "./data/data"
 
-import {Header} from "./components/header/index"
+import {Sneakers} from "./components/sneakers/index"
+import {Item} from "./components/item/index"
 
 
 
-class App extends Component {
-  render() {
+export class App extends React.Component{
+  render(){
     return (
-      <div className="App" /* style= {{}}  */>
-       <Header />
-      </div>
-    );
+        <BrowserRouter>
+           <Switch>
+            <Route  exact path= "/" component= {Sneakers} />
+            <Route path= "/sneaker/:id" component= {Item} />
+          </Switch>
+        </BrowserRouter>
+    )
   }
 }
-
-export default App;
--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Link} from "react-router-dom"
-import styled from "styled-components"
+//import styled from "styled-components"
 
 //import {shoes} from "./data/data"
 
 import {Header} from "../header/index"
 
 
-export class Sneakers extends React.Component{
+export class Item extends React.Component{
     render(){
         return (
             <div>
-                <Header />
-                <h1>sneakers</h1>
-            </div>
+            <Header />
+            <h1>Items</h1>
+        </div>
         )
     }
 }
